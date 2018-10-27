9fbc16a43e052f0c41e1d83e91c0574ce72b81ca
anamika1310 <ganamika875@gmail.com>
2018-10-27 23:51:04 +0530

    Add files via upload

---
+++ parse_out_email_text.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 18 00:37:04 2018
+
+@author: sweety
+"""
+import string
+from nltk.stem.snowball import SnowballStemmer
+
+def parseOutText(f):
+    """ given an opened email file f, parse out all text below the
+        metadata block at the top
+        
+        example use case:
+        f = open("email_file_name.txt", "r")
+        text = parseOutText(f)
+        
+        """
+
+
+    f.seek(0)  ### go back to beginning of file (annoying)
+    all_text = f.read()
+
+    ### split off metadata
+    content = all_text.split("X-FileName:")
+    #print(content)
+    words = ""
+    if len(content) > 1:
+        ### remove punctuation
+        text_string = content[1].translate(str.maketrans("", "",string.punctuation))
+        ### project part 2: comment out the line below
+        #words = text_string
+        stemmer=SnowballStemmer("english")
+        wordsIn = text_string.split(" ")
+        words=[stemmer.stem(words) for words in wordsIn]
+        #print(wordsIn)
+        ### split the text string into individual words, stem each word,
+        ### and append the stemmed word to words (make sure there's a single
+        ### space between each stemmed word)a
+        words=" ".join(words)
+        
+    return words
+
+    
+
+def main():
+    ff = open("test_email.txt", "r")
+    text = parseOutText(ff)
+    print(text)
+
+
+
+if __name__ == '__main__':
+    main()
+
